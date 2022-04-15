79e136dc8f1e13f9eaa50727d8cf9a95c53fc2ca
DigJamOfficialRepo <digambar060985@gmail.com>
2022-04-15 15:22:09 +0800

    Update pom.xml

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -3,33 +3,43 @@ import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
 
+
 public class ApiTest {
     @Test
-    public void checkStatus(){
-        RestAssured.baseURI = "https://681ad72b-5900-4b7e-9e82-bd11b26dadd1.mock.pstmn.io";
-        given()
-                .header ("Content-type","application/json")
-                .queryParam("bookName","theSecret")
-                .when()
-                .get ("/mockApi/get")
-                .then ()
-                .assertThat()
-                .statusCode(200);
+    public void checkStatus() {
+
+        try {
+            RestAssured.baseURI = "https://681ad72b-5900-4b7e-9e82-bd11b26dadd1.mock.pstmn.io";
+            given()
+                    .header("Content-type", "application/json")
+                    .queryParam("bookName", "theSecret")
+                    .when()
+                    .get("/mockApi/get")
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void checkBody(){
-        RestAssured.baseURI = "https://681ad72b-5900-4b7e-9e82-bd11b26dadd1.mock.pstmn.io";
-        given()
-                .header ("Content-type","application/json")
-                .queryParam("bookName","theSecret")
-                .when()
-                .get("/mockApi/get")
-                .then()
-                .assertThat()
-                .body("author",equalTo("Rhonda Byrne"))
-                .body("pages",equalTo("198") )
-                .body("ISBN",equalTo("978-1-58270-170-7"));
+    public void checkBody() {
+        try {
+            RestAssured.baseURI = "https://681ad72b-5900-4b7e-9e82-bd11b26dadd1.mock.pstmn.io";
+            given()
+                    .header("Content-type", "application/json")
+                    .queryParam("bookName", "theSecret")
+                    .when()
+                    .get("/mockApi/get")
+                    .then()
+                    .assertThat()
+                    .body("author", equalTo("Rhonda Byrne"))
+                    .body("pages", equalTo("198"))
+                    .body("ISBN", equalTo("978-1-58270-170-7"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
