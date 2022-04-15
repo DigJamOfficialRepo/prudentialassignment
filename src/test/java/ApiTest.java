2b4800312580b670b55294e842783aa8242349a1
DigJamOfficialRepo <digambar060985@gmail.com>
2022-04-15 13:29:53 +0800

    Mock verification sdded

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -28,7 +28,7 @@ public class ApiTest {
                 .then()
                 .assertThat()
                 .body("author",equalTo("Rhonda Byrne"))
-                .body("pages",equalTo   ("198") )
+                .body("pages",equalTo("198") )
                 .body("ISBN",equalTo("978-1-58270-170-7"));
 
     }
