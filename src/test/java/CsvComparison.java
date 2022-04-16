722ba0fd09cb5a7ef870b1f6aecd0dacc322c955
DigJamOfficialRepo <digambar060985@gmail.com>
2022-04-16 10:29:59 +0800

    Update pom.xml

--- src/test/java/CsvComparison.java
+++ src/test/java/CsvComparison.java
@@ -1,23 +1,34 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.Test;
 
 public class CsvComparison {
     @Test
     public void compareCsvFiles() throws FileNotFoundException{
         String srcline= "", targetline = "";
+        long srclineCount = 0;
+        long targetlineCount = 0;
         boolean isSubset = true;
         try {
             File f1 = new File (System.getProperty("user.dir") + "/src/test/resources/Source.csv");
             File f2 = new File(System.getProperty("user.dir") + "/src/test/resources/Target.csv");
+            srclineCount = countLineJava8(f1.getPath() );
+            targetlineCount  = countLineJava8(f2.getPath());
+            System.out.println("Source file has "+srclineCount+ " records");
+            System.out.println("Target file has " + targetlineCount + " records");
             BufferedReader br = new BufferedReader (new InputStreamReader (new FileInputStream(f1)));
             while ((srcline = br.readLine()) != null){
                 BufferedReader br2 = new BufferedReader(new InputStreamReader(new FileInputStream(f2)));
                 while ((targetline = br2.readLine()) != null){
-                    System.out.println();
-                    System.out.println(srcline);
-                    System.out.println(targetline);
+                    //System.out.println();
+                    //System.out.println(srcline);
+                    //System.out.println(targetline);
                     if (srcline.equals(targetline)) {
                         isSubset = true;
+                        //srclineCount = srclineCount + 1;
                         break;
                     }
                     isSubset = false;
@@ -33,4 +44,26 @@ public class CsvComparison {
         else System.out.println("Source file is not subset of Target file");
 
     }
+
+    public static long countLineJava8(String fileName) {
+
+        Path path = Paths.get(fileName);
+
+        long lines = 0;
+        try {
+
+            // much slower, this task better with sequence access
+            //lines = Files.lines(path).parallel().count();
+
+            lines =
+                    Files.lines(path).count();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
+
+    }
 }
+
